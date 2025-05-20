18ee963aeda793faa55f4b2da41e993a8ce8fced
Luke Kiprop <luke.rono@student.moringaschool.com>
2025-05-20 15:36:00 +0300

    completed lab

--- lib/testing/book_test.py
+++ lib/testing/book_test.py
@@ -31,3 +31,23 @@ class TestBook:
         book.turn_page()
         sys.stdout = sys.__stdout__
         assert(captured_out.getvalue() == "Flipping the page...wow, you read fast!\n")
+        def test_page_count_setter_accepts_integers(self):
+            '''allows setting page_count to an integer value.'''
+            book = Book("1984", 328)
+            book.page_count = 500
+            assert book.page_count == 500
+
+        def test_page_count_setter_rejects_non_integers(self):
+            '''does not allow setting page_count to a non-integer value.'''
+            book = Book("1984", 328)
+            captured_out = io.StringIO()
+            sys.stdout = captured_out
+            book.page_count = "three hundred"
+            sys.stdout = sys.__stdout__
+            assert captured_out.getvalue() == "page_count must be an integer\n"
+            assert book.page_count == 328  # Ensure the value didn't change
+            def test_init_assigns_attributes(self):
+                '''__init__ assigns title and page_count correctly.'''
+                book = Book("To Kill a Mockingbird", 281)
+                assert book.title == "To Kill a Mockingbird"
+                assert book.page_count == 281
